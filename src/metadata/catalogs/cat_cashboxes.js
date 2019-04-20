5b70daec23ab9d8bb73ebf8a10bc49d2a39bd60c
DAlik <alik.datsko@gmail.com>
2019-04-20 21:58:15 +0300

    оо

--- src/metadata/catalogs/cat_cashboxes.js
+++ src/metadata/catalogs/cat_cashboxes.js
@@ -6,7 +6,7 @@
  * Created by Evgeniy Malyarov on 10.04.2018.
  */
 
-function cat_cashboxes($p) {
+export default function ($p) {
 
   // корректируем метаданные - описываем структуру формы элемента справочника
   const meta = $p.cat.cashboxes.metadata();
